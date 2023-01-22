7ec03b824b195ef3bc27454ea08187ce751dc66a
Carlos Oliveira <carlosoliveiramauricio@gmail.com>
2023-01-22 09:36:27 -0300

    t

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {
+  integration: {
     setupNodeEvents(on, config) {},
   },
 })
